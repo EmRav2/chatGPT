d0902c92c9c03ade778b913b2260c89c057fbeb7
Emile Ravelingeen <149885234+EmRav2@users.noreply.github.com>
2024-02-02 15:07:52 +0100

    Refactor: Remove Auth

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -50,7 +50,7 @@ const Chat = () => {
     const [activeCitation, setActiveCitation] = useState<Citation>();
     const [isCitationPanelOpen, setIsCitationPanelOpen] = useState<boolean>(false);
     const abortFuncs = useRef([] as AbortController[]);
-    const [showAuthMessage, setShowAuthMessage] = useState<boolean>(true);
+    const [showAuthMessage, setShowAuthMessage] = useState<boolean>(false);
     const [messages, setMessages] = useState<ChatMessage[]>([])
     const [processMessages, setProcessMessages] = useState<messageStatus>(messageStatus.NotRunning);
     const [clearingChat, setClearingChat] = useState<boolean>(false);
@@ -98,17 +98,15 @@ const Chat = () => {
     }, [appStateContext?.state.chatHistoryLoadingState])
 
     const getUserInfoList = async () => {
-        if (!AUTH_ENABLED) {
-            setShowAuthMessage(false);
-            return;
-        }
-        const userInfoList = await getUserInfo();
-        if (userInfoList.length === 0 && window.location.hostname !== "127.0.0.1") {
-            setShowAuthMessage(true);
-        }
-        else {
-            setShowAuthMessage(false);
-        }
+        setShowAuthMessage(false);
+
+        // const userInfoList = await getUserInfo();
+        // if (userInfoList.length === 0 && window.location.hostname !== "127.0.0.1") {
+        //     setShowAuthMessage(true);
+        // }
+        // else {
+        //     setShowAuthMessage(false);
+        // }
     }
 
     let assistantMessage = {} as ChatMessage
